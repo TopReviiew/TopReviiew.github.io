0dda5abc9bb2085f2be0981bf3f442db06450aef
TopReview <topreviewshelp@gmail.com>
2024-09-07 14:38:59 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle Dark Mode
+function toggleTheme() {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    if (currentTheme === "dark") {
+        document.documentElement.setAttribute("data-theme", "light");
+    } else {
+        document.documentElement.setAttribute("data-theme", "dark");
+    }
+}
