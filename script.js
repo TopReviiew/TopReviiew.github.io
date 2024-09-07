31214d2e48bcb0bd3670e44173f785c6cddcaa5e
TopReview <topreviewshelp@gmail.com>
2024-09-07 19:19:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function toggleTheme() {
         document.documentElement.setAttribute("data-theme", "dark");
     }
 }
+// Get the button
+var backToTopBtn = document.getElementById("backToTop");
+
+// Show the button when the user scrolls down 20px from the top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+}
+
+// Scroll to the top when the user clicks the button
+backToTopBtn.addEventListener('click', function() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+});
